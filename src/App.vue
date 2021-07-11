3464c8623fcb37f760baacff1b237a88c2bed565
unknown <preranadas97@gmail.com>
2021-07-11 12:30:10 +0600

    first commit

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="app">
+    <main class="flexbox">
+    </main>
+  </div>
+</template>
+
+<script>
+
+
+export default {
+  name: 'App',
+  components: {
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/Board.vue
@@ -0,0 +1,11 @@
+<template>
+    <div :id="id"
+    class="board"
+    @dragover.prevent
+    @drop.prevent="drop">
+        </slot>
+    </div>
+</template>
+
+<script>
+</script>
